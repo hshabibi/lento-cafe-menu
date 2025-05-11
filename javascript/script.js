c49cc334fea5e3202157b7173d10a0a25585dbc0
hs <habibi.hst@gmail.com>
2025-05-11 21:41:32 +0330

    Mobile edited

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,11 @@
+//Hamburger
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
 // Smooth scrolling for navigation links
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
